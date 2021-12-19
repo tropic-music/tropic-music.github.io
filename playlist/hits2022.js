1ba222a15df1772aefa5e08813ed4aa7643caae6
Tropic Music <80898777+tropic-music@users.noreply.github.com>
2021-12-19 14:20:14 +0100

    Create hits2022.js

---
+++ playlist/hits2022.js
@@ -0,0 +1,147 @@
+jQuery(function ($) {
+    'use strict'
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+
+        var player = new Plyr('#audio1', {
+            controls: [
+                'restart',
+                'play',
+                'progress',
+                'current-time',
+                'duration',
+                'mute',
+                'volume',
+                'download'
+            ]
+        });
+
+        var index = 0,
+            playing = false,
+            mediaPath = 'https://tropic-music.github.io/hits2022/',
+            extension = '',
+            tracks = [{
+                "track": 1,
+                "name": "Trei degete - Time time",
+                "duration": "2:55",
+                "file": "Trei Degete - Time Time"
+            }, {
+                "track": 2,
+                "name": "Ambiance Skandal - Offishal",
+                "duration": "2:32",
+                "file": "Ambiance Skandal - Offishal"
+            }, {
+                "track": 3,
+                "name": "The weeknd ft Swedish House Mafia - Moth To a Flame",
+                "duration": "3:36",
+                "file": "The weeknd x Swedish House Mafia - Moth To a Flame"
+            }, {
+                "track": 4,
+                "name": "Naps ft GIMS - Best life",
+                "duration": "3:07",
+                "file": "Naps ft GIMS - Best life"
+            }, {
+                "track": 5,
+                "name": "OrelSan - L'odeur de l'essence",
+                "duration": "3:55",
+                "file": "OrelSan - L'odeur de l'essence"
+            }, {
+                "track": 6,
+                "name": "Stromae - Santé",
+                "duration": "3:25",
+                "file": "Stromae - Santé"
+            }],
+            buildPlaylist = $.each(tracks, function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackDuration = value.duration;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                }
+                $('#plList').append('<li> \
+                    <div class="plItem"> \
+                        <span class="plNum">' + trackNumber + '.</span> \
+                        <span class="plTitle">' + trackName + '</span> \
+                        <span class="plLength">' + trackDuration + '</span> \
+                    </div> \
+                </li>');
+            }),
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').on('play', function () {
+                playing = true;
+                npAction.text('Vous écoutez...');
+            }).on('pause', function () {
+                playing = false;
+                npAction.text('En pause...');
+            }).on('ended', function () {
+                npAction.text('En pause...');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').on('click', function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').on('click', function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').on('click', function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+                updateDownload(id, audio.src);
+            },
+            updateDownload = function (id, source) {
+                player.on('loadedmetadata', function () {
+                    $('a[data-plyr="download"]').attr('href', source);
+                });
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+    } else {
+
+        $('.column').addClass('hidden');
+        var noSupport = $('#audio1').text();
+        $('.container').append('<p class="no-support">' + noSupport + '</p>');
+    }
+});
