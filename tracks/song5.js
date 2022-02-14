e268d1537cc8e2823499e8b56b0c1d2961922fec
Tropic Music <80898777+tropic-music@users.noreply.github.com>
2022-02-14 10:15:31 +0100

    Update song5.js

--- tracks/song5.js
+++ tracks/song5.js
@@ -22,9 +22,9 @@ jQuery(function ($) {
             extension = '',
             tracks = [{
                 "track": 1,
-                "name": "Mc Fly & Carlito - Infini +1000",
-                "duration": "2:43",
-                "file": "Mc Fly & Carlito - Infini +1000"
+                "name": "Martin Garix ft Matisse & Sadko et John Martin - Won’t Let You Go",
+                "duration": "3:27",
+                "file": "Martin Garix - Won’t Let You Go"
             }],
             buildPlaylist = $.each(tracks, function(key, value) {
                 var trackNumber = value.track,
