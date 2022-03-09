34dc1f324732ce7a1a1aa309630f3f9b46de7a9b
Tropic Music <80898777+tropic-music@users.noreply.github.com>
2022-03-09 17:48:36 +0100

    Update hits2022.js

--- playlist/hits2022.js
+++ playlist/hits2022.js
@@ -22,71 +22,76 @@ jQuery(function ($) {
             extension = '',
             tracks = [{
                 "track": 1,
+                "name": "Soolking - Suavemente",
+                "duration": "2:30",
+                "file": "Soolking - Suavemente"
+            }, {
+                "track": 2,
+                "name": "Kungs - Clap Your Hands",
+                "duration": "2:50",
+                "file": "Kungs - Clap Your Hands"
+            }, {
+                "track": 3,
                 "name": "Charlie Puth - Light Switch",
                 "duration": "2:58",
                 "file": "Charlie Puth - Light Switch"
             }, {
-                "track": 2,
+                "track": 4,
                 "name": "Martin Garix ft Matisse & Sadko et John Martin - Won’t Let You Go",
                 "duration": "3:27",
                 "file": "Martin Garix - Won’t Let You Go"
             }, {
-                "track": 3,
+                "track": 5,
                 "name": "Mc Fly & Carlito - Infini +1000",
                 "duration": "2:44",
                 "file": "Mc Fly & Carlito - Infini +1000"
             }, {
-                "track": 4,
+                "track": 6,
                 "name": "Angèle ft Damso - Démons",
                 "duration": "3:20",
                 "file": "Angèle ft Damso - Démons"
             }, {
-                "track": 5,
+                "track": 7,
                 "name": "The Weeknd - Sacrifice",
                 "duration": "3:05",
                 "file": "The Weeknd - Sacrifice"
             }, {
-                "track": 6,
+                "track": 8,
                 "name": "Kungs - Lipstick",
                 "duration": "2:51",
                 "file": "Kungs - Lipstick"
             }, {
-                "track": 7,
+                "track": 9,
                 "name": "Robin Schul ft Dennis Lloyd - Young Right Now",
                 "duration": "2:53",
                 "file": "Robin Schulz x Dennis Lloyd - Young Right Now"
             }, {
-                "track": 8,
+                "track": 10,
                 "name": "Message In A Bottle (Colorblast Version)",
                 "duration": "2:31",
                 "file": "Message In A Bottle (Colorblast Version)"
             }, {
-                "track": 9,
+                "track": 11,
                 "name": "Imagine dragons - Enemy",
                 "duration": "2:42",
                 "file": "Imagine dragons - Enemy"
             }, {
-                "track": 10,
-                "name": "Kungs - Regarde-moi",
-                "duration": "2:51",
-                "file": "Kungs - Regarde-moi"
-            },  {
-                "track": 11,
+                "track": 12,
                 "name": "OrelSan - Jour meilleur",
                 "duration": "3:36",
                 "file": "OrelSan - Jour meilleur"
             }, {
-                "track": 12,
+                "track": 13,
                 "name": "Ed Sheeran ft Elton John - Merry Christmas",
                 "duration": "3:18",
                 "file": "Ed Sheeran ft Elton John - Merry Christmas"
             }, {
-                "track": 13,
+                "track": 14,
                 "name": "Gregory Porter - Dry Bones",
                 "duration": "2:55",
                 "file": "Gregory Porter - Dry Bones"
             }, {
-                "track": 14,
+                "track": 15,
                 "name": "The weeknd ft Swedish House Mafia - Moth To a Flame",
                 "duration": "3:36",
                 "file": "The weeknd x Swedish House Mafia - Moth To a Flame"
