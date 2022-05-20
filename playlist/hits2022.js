7f01cdf77bad881c2a9e14296a7f64515607f872
Tropic Music <80898777+tropic-music@users.noreply.github.com>
2022-05-20 15:36:34 +0200

    Update hits2022.js

--- playlist/hits2022.js
+++ playlist/hits2022.js
@@ -22,196 +22,206 @@ jQuery(function ($) {
             extension = '',
             tracks = [{
                 "track": 1,
+                "name": "Celestal ft Cecilia Krull - Out in style",
+                "duration": "2:40",
+                "file": "Celestal ft Cecilia Krull - Out in style"
+            }, {
+                "track": 2,
+                "name": "Kygo ft DNCE - Dancing Feet",
+                "duration": "2:40",
+                "file": "Kygo ft DNCE - Dancing Feet"
+            }, {
+                "track": 3,
+                "name": "Goerge Ezra - Anyone For You",
+                "duration": "2:40",
+                "file": "Goerge Ezra - Anyone For You"
+            }, {
+                "track": 4,
                 "name": "Dorely - Où que tu sois",
                 "duration": "2:40",
                 "file": "Dorely - Où que tu sois"
             }, {
-                "track": 2,
+                "track": 5,
                 "name": "Jax Jones ft MNEK - Where Did You Go",
                 "duration": "2:54",
                 "file": "Jax Jones ft MNEK - Where Did You Go"
             }, {
-                "track": 3,
+                "track": 6,
                 "name": "Imanbek & BYOR - Belly Dancer",
                 "duration": "2:21",
                 "file": "Imanbek & BYOR - Belly Dancer"
             }, {
-                "track": 4,
+                "track": 7,
                 "name": "Sound of legend - Maniac",
                 "duration": "2:07",
                 "file": "Sound of legend - Maniac"
             }, {
-                "track": 5,
+                "track": 8,
                 "name": "David Guetta ft Becky Hill ft Ella Henderson - Crazy what love can do",
                 "duration": "2:38",
                 "file": "David Guetta ft Becky Hill ft Ella Henderson - Crazy what love can do"
             }, {
-                "track": 6,
+                "track": 9,
                 "name": "Lujipeka - Pas à ma place",
                 "duration": "2:44",
                 "file": "Lujipeka - Pas à ma place"
             }, {
-                "track": 7,
+                "track": 10,
                 "name": "Angèle - Libre",
                 "duration": "2:37",
                 "file": "Angèle - Libre"
             }, {
-                "track": 8,
+                "track": 11,
                 "name": "Harry Style - As It Was",
                 "duration": "2:37",
                 "file": "Harry Style - As It Was"
             }, {
-                "track": 9,
+                "track": 12,
                 "name": "Purple disco machine - In The Dark",
                 "duration": "2:56",
                 "file": "Purple disco machine - In The Dark"
             }, {
-                "track": 10,
+                "track": 13,
                 "name": "Feder - Strangers",
                 "duration": "2:45",
                 "file": "Feder - Strangers"
             }, {
-                "track": 11,
+                "track": 14,
                 "name": "Kikesa ft Soprano - Tout ira bien",
                 "duration": "2:30",
                 "file": "Kikesa ft Soprano - Tout ira bien"
             }, {
-                "track": 12,
+                "track": 15,
                 "name": "Camila cabello ft Ed sheeran - Bam Bam",
                 "duration": "3:22",
                 "file": "Camila cabello ft Ed sheeran - Bam Bam"
             }, {
-                "track": 13,
+                "track": 16,
                 "name": "Clara Luciani - Amour toujours",
                 "duration": "3:10",
                 "file": "Clara Luciani - Amour toujours"
             }, {
-                "track": 14,
+                "track": 17,
                 "name": "Willy Wiliams - Trompeta",
                 "duration": "2:29",
                 "file": "Willy Wiliams - Trompeta"
             }, {
-                "track": 15,
+                "track": 18,
                 "name": "Soprano - NKOTB",
                 "duration": "3:30",
                 "file": "Soprano - NKOTB"
             }, {
-                "track": 16,
+                "track": 19,
                 "name": "Adèle - Oh My God",
                 "duration": "3:41",
                 "file": "Adèle - Oh My God"
             }, {
-                "track": 17,
+                "track": 20,
                 "name": "Yanns - Clic clic pan pan",
                 "duration": "2:34",
                 "file": "Yanns - Clic clic pan pan"
             }, {
-                "track": 18,
+                "track": 21,
                 "name": "Orelsan - La Quête",
                 "duration": "3:44",
                 "file": "Orelsan - La Quête"
             }, {
-                "track": 19,
+                "track": 22,
                 "name": "Ridsa - Santa Maria",
                 "duration": "2:56",
                 "file": "Ridsa - Santa Maria"
             }, {
-                "track": 20,
+                "track": 23,
                 "name": "Ownboss ft Sevek - Move Your Body",
                 "duration": "2:18",
                 "file": "Ownboss ft Sevek - Move Your Body"
             }, {
-                "track": 21,
+                "track": 24,
                 "name": "Keen'v - Affaire classée",
                 "duration": "2:42",
                 "file": "Keen'v - Affaire classée"
             }, {
-                "track": 22,
+                "track": 25,
                 "name": "Inna ft Sean Paul - UP",
                 "duration": "2:27",
                 "file": "Inna ft Sean Paul - UP"
             }, {
-                "track": 23,
+                "track": 26,
                 "name": "Tayc - Dodo",
                 "duration": "2:54",
                 "file": "Tayc - Dodo"
             }, {
-                "track": 24,
+                "track": 27,
                 "name": "Pepas ft DJ Adoni - El Incomprendido",
                 "duration": "3:45",
                 "file": "Pepas ft DJ Adoni - El Incomprendido"
             }, {
-                "track": 25,
+                "track": 28,
                 "name": "CKay - Emiliana",
                 "duration": "2:30",
                 "file": "CKay - Emiliana"
             }, {
-                "track": 26,
+                "track": 29,
                 "name": "Soolking - Suavemente",
                 "duration": "2:30",
                 "file": "Soolking - Suavemente"
             }, {
-                "track": 27,
+                "track": 30,
                 "name": "Kungs - Clap Your Hands",
                 "duration": "2:50",
                 "file": "Kungs - Clap Your Hands"
             }, {
-                "track": 28,
-                "name": "Charlie Puth - Light Switch",
-                "duration": "2:58",
-                "file": "Charlie Puth - Light Switch"
-            }, {
-                "track": 29,
+                "track": 31,
                 "name": "Martin Garix ft Matisse & Sadko et John Martin - Won’t Let You Go",
                 "duration": "3:27",
                 "file": "Martin Garix - Won’t Let You Go"
             }, {
-                "track": 30,
+                "track": 32,
                 "name": "Mc Fly & Carlito - Infini +1000",
                 "duration": "2:44",
                 "file": "Mc Fly & Carlito - Infini +1000"
             }, {
-                "track": 31,
+                "track": 33,
                 "name": "Angèle ft Damso - Démons",
                 "duration": "3:20",
                 "file": "Angèle ft Damso - Démons"
             }, {
-                "track": 32,
+                "track": 34,
                 "name": "The Weeknd - Sacrifice",
                 "duration": "3:05",
                 "file": "The Weeknd - Sacrifice"
             }, {
-                "track": 33,
+                "track": 35,
                 "name": "Kungs - Lipstick",
                 "duration": "2:51",
                 "file": "Kungs - Lipstick"
             }, {
-                "track": 34,
+                "track": 36,
                 "name": "Robin Schul ft Dennis Lloyd - Young Right Now",
                 "duration": "2:53",
                 "file": "Robin Schulz x Dennis Lloyd - Young Right Now"
             }, {
-                "track": 35,
+                "track": 37,
                 "name": "Message In A Bottle (Colorblast Version)",
                 "duration": "2:31",
                 "file": "Message In A Bottle (Colorblast Version)"
             }, {
-                "track": 36,
+                "track": 38,
                 "name": "Imagine dragons - Enemy",
                 "duration": "2:42",
                 "file": "Imagine dragons - Enemy"
             }, {
-                "track": 37,
+                "track": 39,
                 "name": "OrelSan - Jour meilleur",
                 "duration": "3:36",
                 "file": "OrelSan - Jour meilleur"
             }, {
-                "track": 38,
+                "track": 40,
                 "name": "Gregory Porter - Dry Bones",
                 "duration": "2:55",
                 "file": "Gregory Porter - Dry Bones"
             }, {
-                "track": 39,
+                "track": 41,
                 "name": "The weeknd ft Swedish House Mafia - Moth To a Flame",
                 "duration": "3:36",
                 "file": "The weeknd x Swedish House Mafia - Moth To a Flame"
