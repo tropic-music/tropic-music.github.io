b573b24beb98290cdccb556e75d37d68b5a73217
Tropic Music <80898777+tropic-music@users.noreply.github.com>
2022-05-24 09:19:46 +0200

    Update soirée.js

--- playlist/soirée.js
+++ playlist/soirée.js
@@ -22,154 +22,129 @@ jQuery(function ($) {
             extension = '',
             tracks = [{
                 "track": 1,
-                "name": "Maroon 5 - Animals",
-                "duration": "3:46",
-                "file": "Maroon 5 - Animals"
-            }, {
-                "track": 2,
                 "name": "Gims - Bella",
                 "duration": "3:15",
                 "file": "Gims - Bella"
             }, {
-                "track": 3,
+                "track": 2,
                 "name": "Justin Timberlake - Can't stop the feeling",
                 "duration": "3:38",
                 "file": "Justin Timberlake - Can't stop the feeling"
             }, {
-                "track": 4,
+                "track": 3,
                 "name": "Soprano - En feu",
                 "duration": "3:18",
                 "file": "Soprano - En feu"
             }, {
-                "track": 5,
+                "track": 4,
                 "name": "Psy - Gangnam Style",
                 "duration": "3:25",
                 "file": "Psy - Gangnam Style"
             }, {
-                "track": 6,
+                "track": 5,
                 "name": "Black Eyed Peas - I Gotta Feeling",
                 "duration": "3:30",
                 "file": "Black Eyed Peas - I Gotta Feeling"
             }, {
-                "track": 7,
-                "name": "Martin Garix ft Bebe Rehxa - In the Name of Love",
-                "duration": "3:00",
-                "file": "Martin Garix ft Bebe Rehxa - In the Name of Love"
-            }, {
-                "track": 8,
+                "track": 6,
                 "name": "Ofenbach - Katchi",
                 "duration": "4:06",
                 "file": "Ofenbach - katchi"
             }, {
-                "track": 9,
+                "track": 7,
                 "name": "Kendji Girac - Color Gitano",
                 "duration": "2:57",
                 "file": "Kendji Girac - Color Gitano"
             }, {
-                "track": 10,
+                "track": 8,
                 "name": "Soprano - le coach feat vincenzo",
                 "duration": "3:14",
                 "file": "Soprano - le coach feat vincenzo"
             }, {
-                "track": 11,
+                "track": 9,
                 "name": "DJ Snake ft Justin Bieber - Let Me Love You",
                 "duration": "3:03",
                 "file": "DJ Snake ft Justin Bieber - Let Me Love You"
             }, {
-                "track": 12,
+                "track": 10,
                 "name": "Calvin Harris - My Way",
                 "duration": "3:28",
                 "file": "Calvin Harris - My Way"
             }, {
-                "track": 13,
+                "track": 11,
                 "name": "Stromae - Papaoutai",
                 "duration": "3:18",
                 "file": "Stromae - Papaoutai"
             }, {
-                "track": 14,
+                "track": 12,
                 "name": "Ed Sheeran - Shape of you",
                 "duration": "3:53",
                 "file": "Ed Sheeran - shape of you"
             }, {
-                "track": 15,
+                "track": 13,
                 "name": "Black M - Sur ma route",
                 "duration": "3:42",
                 "file": "Black M - Sur ma route"
             }, {
-                "track": 16,
+                "track": 14,
                 "name": "DJ Snake ft Selena Gomez, Ozuna, Cardi B - Taki Taki",
                 "duration": "3:12",
                 "file": "DJ Snake ft Selena Gomez, Ozuna, Cardi B - Taki Taki"
             }, {
-                "track": 17,
-                "name": "Madeon - All My Friends",
-                "duration": "2:59",
-                "file": "Madeon - All My Friends"
-            }, {
-                "track": 18,
+                "track": 15,
                 "name": "Vicetone - Astronomia",
                 "duration": "3:18",
                 "file": "Vicetone - Astronomia"
             }, {
-                "track": 19,
+                "track": 16,
                 "name": "David Guetta & Showtek - Bad ft.Vassy",
                 "duration": "2:39",
                 "file": "David Guetta & Showtek - Bad ft.Vassy"
             }, {
-                "track": 20,
+                "track": 17,
                 "name": "David Guetta ft MORTEN - Nothing",
                 "duration": "2:48",
                 "file": "David Guetta ft MORTEN - Nothing"
             }, {
-                "track": 21,
+                "track": 18,
                 "name": "Calvin Harris - Summer",
                 "duration": "3:32",
                 "file": "Calvin Harris - Summer"
             }, {
-                "track": 22,
+                "track": 19,
                 "name": "Indochine - L'aventurier",
                 "duration": "3:31",
                 "file": "Indochine - l'aventurier"
             }, {
-                "track": 23,
+                "track": 20,
                 "name": "Image - Les demons de minuit",
                 "duration": "3:53",
                 "file": "Image - Les demons de minuit"
             }, {
-                "track": 24,
+                "track": 21,
                 "name": "Debut de soiree - Nuit de folie",
                 "duration": "3:47",
                 "file": "Debut de soiree - Nuit de folie"
             }, {
-                "track": 25,
+                "track": 22,
                 "name": "Partenaire Particulier - Partenaire particulier",
                 "duration": "3:51",
                 "file": "Partenaire Particulier - Partenaire particulier"
             }, {
-                "track": 26,
+                "track": 23,
                 "name": "Téléphone - ça c'est vraiment toi",
                 "duration": "4:26",
                 "file": "Téléphone - ça c'est vraiment toi"
             }, {
-                "track": 27,
+                "track": 24,
                 "name": "Yanns - Clic clic pan pan",
                 "duration": "2:34",
                 "file": "Yanns - Clic clic pan pan"
             }, {
-                "track": 28,
-                "name": "Vroum Vroum - Moha K",
-                "duration": "2:56",
-                "file": "Vroum Vroum - Moha K"
-            }, {
-                "track": 29,
+                "track": 25,
                 "name": "Jul - Ça tourne dans ma tête",
                 "duration": "2:55",
                 "file": "Jul - Ça tourne dans ma tête"
-            }, {
-                "track": 30,
-                "name": "L'Algérino - Sensación",
-                "duration": "2:42",
-                "file": "L'Algérino - Sensación"
             }],
             buildPlaylist = $.each(tracks, function(key, value) {
                 var trackNumber = value.track,
